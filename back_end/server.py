0fc4d8325e107ab995c803fa2c925d5bd3141186
dkyg1087 <43636582+dkyg1087@users.noreply.github.com>
2022-10-08 22:33:41 -0500

    finished frontend and backend

--- back_end/server.py
+++ back_end/server.py
@@ -1,6 +1,42 @@
+import websockets
+import asyncio
 import picar_4wd as fc
-import socket
+import time
 
-HOST = "172.16.109.23" # IP address of your Raspberry PI
-PORT = 12345        # The port used by the server
 
+speed = 20
+distance = 0
+fc.start_speed_thread()
+
+async def handler(websocket):
+    global speed,distance
+    async for message in (websocket):
+        if message == "forward":
+            fc.stop()
+            fc.forward(speed)
+        elif message == "backward":
+            fc.stop()
+            fc.backward(speed)
+        elif message == "left":
+            fc.stop()
+            fc.turn_left(speed)
+        elif message == "right":
+            fc.stop()
+            fc.turn_right(speed)
+        elif message == "stop":
+            fc.stop()
+        elif message.startswith("#"):
+            print("Text recieved from client : ",message[1:])
+        elif message == "query":
+            b = round(fc.utils.power_read(),2)
+            t = round(fc.utils.cpu_temperature(),2)
+            s = round(fc.speed_val(),2)
+            distance+= round(s*0.1,2)
+            #print(b,t,s,distance)
+            await websocket.send(str(s)+","+str(distance)+","+str(t)+","+str(b))
+
+async def main():
+    async with websockets.serve(handler,"172.16.109.23",8564):
+        await asyncio.Future()
+
+asyncio.run(main())
--- front_end/socket.js
+++ front_end/socket.js
@@ -1,17 +1,45 @@
-var socket = new WebSocket('ws://172.16.109.23:12346');
+var socket
+var interval
 window.addEventListener('load', (event) => {
-    try{
-        socket.addEventListener('open', (event) => {
-            socket.send('Hello Server!');
-        });
-        socket.addEventListener('message', (event) => {
-            alert('Message from server ', event.data);
-        });
-    }
-    catch(err){
-        alert(' Socket connection failed, please reload')
-    }
+    socket = new WebSocket('ws://172.16.109.23:8564');
+    socket.addEventListener('open', (event) => {
+        socket.send('#Hello Server!');
+    });
+    socket.addEventListener('message', (event) => {
+        let str = event.data
+        const myArr = str.split(',')
+        document.getElementById("Speed").innerText = myArr[0]
+        document.getElementById("Distance").innerText = myArr[1]
+        document.getElementById("Temperature").innerText = myArr[2]
+        document.getElementById("Battery").innerText = myArr[3]
+    });
+    socket.addEventListener('error', (event) => {
+        let reload = confirm(' Socket connection failed,do you want to reload?')
+        if (reload){
+            window.location.reload()
+        }
+    })
+    socket.addEventListener('close', () => {
+        clearInterval(interval)
+        let reload = confirm(' Socket connection failed,do you want to reload?')
+        if (reload){
+            window.location.reload()
+        }
+    });
+
+    interval = setInterval(function(){
+        socket.send("query");
+    }, 50);
   });
+
+function message_send(){
+    socket.send("#"+document.getElementById('input').value)
+    document.getElementById('input').value = ""
+}
+
+function send_cmd(str){
+    socket.send(str)
+}
 
 //socket.send('Hello Server!');
 //   socket.send('Hello Server!');
